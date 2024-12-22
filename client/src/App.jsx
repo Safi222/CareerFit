6aaef437bd5255365593d189caa18afce13cb547
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2024-12-22 02:56:01 +0200

    updataing image in jobs and fixed src ,and button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-import Search from './components/SearchD/Search'
-import Jobs from './components/JobsD/Jobs'
-import Value from './components/ValueD/Value'
-import Footer from './components/FooterD/Footer'
-import Slogan from './components/SloganD/Slogan';
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Search from "./components/SearchD/Search";
+import Jobs from "./components/JobsD/Jobs";
+import Value from "./components/ValueD/Value";
+import Footer from "./components/FooterD/Footer";
+import Slogan from "./components/SloganD/Slogan";
 const App = () => {
   return (
-    <div className='w-[85%] m-auto bg-white'>
-      <Navbar/>
+    <div className="w-[85%] m-auto bg-white">
+      <Navbar />
       <Slogan />
-      <Search/>
-      <Jobs/>
-      <Value/>
-      <Footer/>
+      <Search />
+      <Jobs />
+      <Value />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- client/src/components/JobsD/Jobs.jsx
+++ client/src/components/JobsD/Jobs.jsx
@@ -1,74 +1,80 @@
-import React from 'react'
-import {BiTimeFive} from 'react-icons/bi';
-import logo1 from '../../Assets/logo1.png';
-import logo2 from '../../Assets/logo2.png';
-import logo3 from '../../Assets/logo3.png';
+import React from "react";
+import { BiTimeFive } from "react-icons/bi";
+import logo1 from "../../Assets/logo1.png";
+import logo2 from "../../Assets/logo2.png";
+import logo3 from "../../Assets/logo3.png";
 
-const Data = [{
-    id:1,
+const Data = [
+  {
+    id: 1,
     image: logo1,
-    title: 'Graphic Designer',
-    time: 'Now',
-    location: 'Haiti',
-    desc: 'lorem ipusm, dolor sit amet consectur adipisicing elit. Corrupti, laboriosam!',
-    company: 'Vertex solutions Co.'
-},
-{
-    id:2,
+    title: "Graphic Designer",
+    time: "Now",
+    location: "Haiti",
+    desc: "lorem ipusm, dolor sit amet consectur adipisicing elit. Corrupti, laboriosam!",
+    company: "Vertex solutions Co.",
+  },
+  {
+    id: 2,
     image: logo2,
-    title: 'Business analyst',
-    time: '3Hrs',
-    location: 'US',
-    desc: 'lorem ipusm, dolor sit amet consectur adipisicing elit. Corrupti, laboriosam!',
-    company: 'Merci Co.'
-},
-{
-    id:3,
+    title: "Business analyst",
+    time: "3Hrs",
+    location: "US",
+    desc: "lorem ipusm, dolor sit amet consectur adipisicing elit. Corrupti, laboriosam!",
+    company: "Merci Co.",
+  },
+  {
+    id: 3,
     image: logo3,
-    title: 'Backend developer',
-    time: '20Hrs',
-    location: 'Austria',
-    desc: 'lorem ipusm, dolor sit amet consectur adipisicing elit. Corrupti, laboriosam!',
-    company: 'Noka Tech Co.'
-}
-]
+    title: "Backend developer",
+    time: "20Hrs",
+    location: "Austria",
+    desc: "lorem ipusm, dolor sit amet consectur adipisicing elit. Corrupti, laboriosam!",
+    company: "Noka Tech Co.",
+  },
+];
 const Jobs = () => {
   return (
     <div>
-    <div className="jobContainer flex gap-10 justify-center flex-wrap items-center py-10">
-        
-    {
-        Data.map(({id, image, title, time, location, desc, company}) =>{
-            return(
-            <div key={id} className="group group/item singleJob w-[250px] p-[20px] bg-white rounded[10px] hover:bg-orangeColor shadow-lg shadow-greyIsh-400/700 hover:shadow-lg">
-            
-            <span className='flex justify-between items-center gap-4'>
-                <h1 className='text-[16px] font-semibold text-textColor group-hover:text-white'>{title}</h1>
-                <span className='flex items-center text-[#ccc] gap-1'>
-                    <BiTimeFive/>{time}
+      <div className="jobContainer flex gap-10 justify-center flex-wrap items-center py-10">
+        {Data.map(({ id, image, title, time, location, desc, company }) => {
+          return (
+            <div
+              key={id}
+              className="group group/item singleJob w-[250px] p-[20px] bg-white rounded[10px] hover:bg-orangeColor shadow-lg shadow-greyIsh-400/700 hover:shadow-lg"
+            >
+              <span className="flex justify-between items-center gap-4">
+                <h1 className="text-[15px] font-semibold text-textColor group-hover:text-white">
+                  {title}
+                </h1>
+                <span className="flex items-center text-[#ccc] gap-1">
+                  <BiTimeFive />
+                  {time}
                 </span>
-            </span>
+              </span>
 
-            <h6 className='text-[#ccc]'>{location}</h6>
+              <h6 className="text-[#ccc]">{location}</h6>
 
-            <p className='text-[13px] text-[#959595] pt-[20px] border-t-[2px] mt-[20px] group-hover:text-white'>
+              <p className="text-[13px] text-[#959595] pt-[20px] border-t-[2px] mt-[20px] group-hover:text-white">
                 {desc}
-            </p>
-            
-            <div className='company flex items-center gap-2'>
-                <img src="{image}" alt="Company logo" className='w-[10%]' />
-                <span className='text-[14px] py-[1rem] block group-hover:text-white'>{company}</span>
-            </div>
+              </p>
 
-            <button className='border-[2px] rounded-[10px] block p-[10px] w-full text-[14px] font-semibold text-textColor hover:bg-white group-hover/item:text-textColor group-hover:text-white'>Fit in Now</button>
-            
-            </div>    
-            )
-        })
-    }
-        </div>
-    </div> 
-  )
-}
+              <div className="company flex items-center gap-2">
+                <img src={image} alt="Company logo" className="w-[10%]" />
+                <span className="text-[14px] py-[1rem] block group-hover:text-white">
+                  {company}
+                </span>
+              </div>
+
+              <button className="border-[2px] rounded-[10px] block p-[10px] w-full text-[14px] font-semibold text-textColor hover:bg-white group-hover/item:text-textColor group-hover:text-orangeColor">
+                Fit in Now
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default Jobs;
