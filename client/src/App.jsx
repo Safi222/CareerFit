e3d0db935c610200f4b9f645d3a335fe3a6f632c
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2024-12-24 22:06:48 +0200

    add routes to App.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,51 @@
-import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Search from "./components/SearchD/Search";
 import Jobs from "./components/JobsD/Jobs";
 import Value from "./components/ValueD/Value";
 import Footer from "./components/FooterD/Footer";
 import Slogan from "./components/SloganD/Slogan";
+import Login from "./components/Login/Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 const App = () => {
   return (
-    <div className="w-[85%] m-auto bg-white">
-      <Navbar />
-      <Slogan />
-      <Search />
-      <Jobs />
-      <Value />
-      <Footer />
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="w-[85%] m-auto bg-white">
+              <Navbar />
+              <Slogan />
+              <Search />
+              <Jobs />
+              <Value />
+              <Footer />
+            </div>
+          }
+        />
+        <Route
+          path="/jobs"
+          element={
+            <div className="w-[85%] m-auto bg-white">
+              <Navbar />
+              <Jobs />
+              <Footer />
+            </div>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <div className="w-[85%] m-auto bg-white">
+              <Navbar />
+              <Login />
+              <Footer />
+            </div>
+          }
+        />
+      </Routes>
+    </Router>
   );
 };
 
