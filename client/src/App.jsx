30d364ca7212f91648390f646c93bb308ceb91a4
Safi222 <safia2000gibril@gmail.com>
2024-12-29 14:49:29 +0300

    some files correction

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,6 @@ import Register from "./components/Register/Register";
 import AboutUs from "./components/About US/AboutUs";
 import CvPilot from "./components/CvPilot/CvPilot";
 import PreCvPilot from "./components/CvPilot/PreCvPilot";
-import Dashboard from "./components/Dashboard/dashboard";
 
 const App = () => {
   return (
@@ -91,18 +90,7 @@ const App = () => {
             </div>
           }
         />
-        
-        <Route
-          path="/dashboard/:userId"
-          element={
-            <div className="w-[85%] m-auto bg-white">
-              <Navbar />
-              <Dashboard />
-              <Footer />
-            </div>
-          }
-        />
-      </Routes>
+        </Routes>
     </Router>
   );
 };
