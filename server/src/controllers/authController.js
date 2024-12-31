73b38bf80c93ba67782b780b63ba3dc9d872a4b4
Oxshady <shadymahmod55@gmail.com>
2024-12-31 10:34:28 +0200

    feat: create register controller to handle user registration and token generation
    - Implemented user registration logic in the registerController.
    - Hash password using bcrypt before saving the user.
    - Generate a JWT token with user details and a 1-day expiration.
    - Return success response with the generated token.

---
+++ server/src/controllers/authController.js
@@ -0,0 +1,23 @@
+const bcrypt = require('bcrypt')
+const User = require('../models/User');
+const jwt = require('jsonwebtoken')
+const registerController = async (req, res) => {
+	const { firstName, lastName, email, password } = req.body;
+	const hashedPassword = await bcrypt.hash(password, 10);
+	const user = new User({
+		firstName,
+		lastName,
+		email,
+		password: hashedPassword
+	})
+	await user.save()
+	token = await jwt.sign({ firstName: user.firstName, id: user._id }, "bla bla", { expiresIn: '1d' })
+	return res.status(201).json(
+		{
+			status: "success",
+			data: {
+				token
+			}
+		}
+	)
+}
