45c15b128ddbf36862b40fef3e154e7f361d7a48
Oxshady <shadymahmod55@gmail.com>
2024-12-31 19:24:00 +0200

    update model to be compatible with OAuth

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -5,21 +5,17 @@ const userSchema = new mongoose.Schema(
   {
     firstName: {
       type: String,
-      required: [true, 'First Name is required'],
     },
     lastName: {
         type: String,
-        required: [true, 'Last Name is required'],
     },
     email: {
       type: String,
-      required: [true, 'Email is required'],
       unique: true,
       match: [/\S+@\S+\.\S+/, 'Invalid email format'],
     },
     password: {
       type: String,
-      required: [true, 'Password is required'],
       minlength: 6,
     },
     profilePic: {
@@ -34,17 +30,14 @@ const userSchema = new mongoose.Schema(
           ref: 'Job', // Reference to the Job model
         },
     ],
+    googleId: {
+      type: String,
+      unique: true
+    }
   },
   { timestamps: true }
 );
 
-// Hash password before saving
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
 
 // Compare password
 userSchema.methods.matchPassword = async function (enteredPassword) {
