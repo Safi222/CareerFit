384babf84dce626435c810119b5e4748f60386d2
Oxshady <shadymahmod55@gmail.com>
2024-12-31 10:58:34 +0200

    fix: improve user registration flow with validation and email check
    - Added validation to check for errors in user input using express-validator.
    - Implemented a check to ensure the email is not already registered.
    - Enhanced the response to handle validation and duplicate email cases properly.
    - Updated the JWT token generation to include 'Bearer' prefix in the response.

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,8 +1,27 @@
 const bcrypt = require('bcrypt')
 const User = require('../models/User');
 const jwt = require('jsonwebtoken')
+const validation = require('express-validator')
 const registerController = async (req, res) => {
+	const errors = validation.validationResult(req)
+	if (!errors.isEmpty()){
+		return res.status(400).json({
+			status : "fail",
+			"data" : errors.array()[0]
+		})
+	}
 	const { firstName, lastName, email, password } = req.body;
+	const oldUser = await User.findOne({email: email})
+	if (oldUser){
+		return res.status(400).json(
+			{
+				status: "success",
+				data: {
+					title: "cannot use that email"
+				}
+			}
+		)
+	}
 	const hashedPassword = await bcrypt.hash(password, 10);
 	const user = new User({
 		firstName,
@@ -11,7 +30,7 @@ const registerController = async (req, res) => {
 		password: hashedPassword
 	})
 	await user.save()
-	token = await jwt.sign({ firstName: user.firstName, id: user._id }, "bla bla", { expiresIn: '1d' })
+	token = 'Bearer ' + await jwt.sign({ firstName: user.firstName, id: user._id }, "bla bla", { expiresIn: '1d' })
 	return res.status(201).json(
 		{
 			status: "success",
