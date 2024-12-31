bdff3035d7557509a31771c29ccdb5a0fc043b8c
Oxshady <shadymahmod55@gmail.com>
2024-12-31 19:25:50 +0200

    Set up Google OAuth routes. Implement authentication and callback handling for Google login.

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,4 +1,6 @@
 const Route = require('express').Router
+const passport = require('passport')
+const passportSetup = require('../config/passportSetup')
 const authController = require('../controllers/authController')
 const validatorMiddleware = require('../middlewares/validatorMiddleware')
 const auth = Route()
@@ -6,5 +8,11 @@ const auth = Route()
 auth.post('/register', validatorMiddleware.registerValidator(), authController.registerController)
 
 auth.post('/login', validatorMiddleware.loginValidator(), authController.loginController)
+
+auth.get('/google',
+	passport.authenticate('google', { scope: ['profile'] }));
+
+auth.get('/google/callback',
+	passport.authenticate('google', { failureRedirect: '/login' }), authController.googleAuth);
 
 module.exports = auth
