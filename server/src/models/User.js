39435c4256ee28de4eb591f550c8733daa48761f
Oxshady <138263957+Oxshady@users.noreply.github.com>
2025-01-01 08:01:09 +0200

    updating model to solve conflict ofb dupicate email causes of db validation and making validation on route itself

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,47 +1,40 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
-const userSchema = new mongoose.Schema(
-  {
+const userSchema = new mongoose.Schema({
     firstName: {
-      type: String,
+        type: String,
     },
     lastName: {
         type: String,
     },
     email: {
-      type: String,
-      unique: true,
-      match: [/\S+@\S+\.\S+/, 'Invalid email format'],
+        type: String,
+        match: [/\S+@\S+\.\S+/, 'Invalid email format'],
     },
     password: {
-      type: String,
-      minlength: 6,
+        type: String,
+        minlength: 6,
     },
     profilePic: {
-      type: String, // URL to Cloudinary
+        type: String, // URL to Cloudinary
     },
     cv: {
-      type: String, // URL to Cloudinary
+        type: String, // URL to Cloudinary
     },
-    appliedJobs: [
-        {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: 'Job', // Reference to the Job model
-        },
-    ],
+    appliedJobs: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Job', // Reference to the Job model
+    }, ],
     googleId: {
-      type: String,
-      unique: true
+        type: String,
     }
-  },
-  { timestamps: true }
-);
+}, { timestamps: true });
 
 
 // Compare password
-userSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
+userSchema.methods.matchPassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
 };
 
 module.exports = mongoose.model('User', userSchema);
