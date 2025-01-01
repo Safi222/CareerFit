1907c779bb80238645b54ae8377f950decfc1013
Oxshady <138263957+Oxshady@users.noreply.github.com>
2025-01-01 08:00:50 +0200

    updating model to solve conflict ofb dupicate email causes of db validation and making validation on route itself

--- server/src/config/passportSetup.js
+++ server/src/config/passportSetup.js
@@ -2,21 +2,22 @@ const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('../models/User')
 passport.use(new GoogleStrategy({
-	clientID: process.env.CLIENT_ID,
-	clientSecret: process.env.CLIENT_SECRET,
-	callbackURL: "/api/auth/google/callback"
-},
-	async function (accessToken, refreshToken, profile, cb) {
-		const currentUser = await User.findOne({googleId: profile.id})
-		if (!currentUser){
-			const newUser = new User({
-				firstName: profile.name.givenName,
-				lastName: profile.name.familyName,
-				googleId: profile.id
-			})
-			await newUser.save()
-			cb(null, newUser)
-		}
-		cb(null, currentUser)
-	}
+        clientID: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        callbackURL: "/api/auth/google/callback"
+    },
+    async function(accessToken, refreshToken, profile, cb) {
+        const currentUser = await User.findOne({ googleId: profile.id })
+        if (currentUser === null) {
+            const newUser = new User({
+                firstName: profile.name.givenName,
+                lastName: profile.name.familyName,
+                googleId: profile.id,
+                profilePic: profile.photos[0].value
+            })
+            await newUser.save()
+            return cb(null, newUser)
+        }
+        cb(null, currentUser)
+    }
 ));
