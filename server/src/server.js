e6bca38b3cc117ad48db8bf7ed902eb65651ed9d
Oxshady <shadymahmod55@gmail.com>
2025-01-02 11:21:27 +0200

    feat(app): Integrate job search route into main application
    - Integrated `jobFind` router into the main application using `app.use('/api/jobs', jobFind)`.
    - This allows job search functionality to be accessed via `/api/jobs/jobSearching` route.
    - Enhances application structure by consolidating related routes under `/api/jobs`.
    
    This integration ensures better route management and keeps related functionalities grouped.

--- server/src/server.js
+++ server/src/server.js
@@ -2,12 +2,14 @@ require('dotenv').config(); // Load environment variables
 const express = require('express');
 const connectDatabase = require('./config/database');
 const auth = require("./routes/authRoutes")
+const jobFind = require('./routes/jobRoutes')
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
 app.use('/api/auth', auth)
+app.use('/api/jobs', jobFind)
 // Connect to MongoDB
 connectDatabase();
 
