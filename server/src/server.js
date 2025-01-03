9a88d40222395dc6e12c4bae77b4a0f01119c0a9
Ahmed Harhash <ahmedharhash0048@gmail.com>
2025-01-04 00:27:02 +0200

    added cors

--- server/src/server.js
+++ server/src/server.js
@@ -1,17 +1,28 @@
 require('dotenv').config(); // Load environment variables
 const express = require('express');
+const cors = require('cors'); // Import CORS middleware
 const connectDatabase = require('./config/database');
-const auth = require("./routes/authRoutes")
-const jobRoutes = require('./routes/jobRoutes')
-const userRoutes = require('./routes/userRoutes')
+const auth = require("./routes/authRoutes");
+const jobRoutes = require('./routes/jobRoutes');
+const userRoutes = require('./routes/userRoutes');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// CORS Configuration
+const corsOptions = {
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173', // Replace with your frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+};
+app.use(cors(corsOptions)); // Use CORS middleware
+
 // Middleware
 app.use(express.json());
-app.use('/api/auth', auth)
-app.use('/api/jobs', jobRoutes)
-app.use('/api/users', userRoutes)
+app.use('/api/auth', auth);
+app.use('/api/jobs', jobRoutes);
+app.use('/api/users', userRoutes);
+
 // Connect to MongoDB
 connectDatabase();
 
