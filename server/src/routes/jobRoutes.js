f46306f587fbdbfb834b2ad2b2adce182a5c4a36
Ahmed Harhash <ahmedharhash0048@gmail.com>
2025-01-04 02:36:23 +0200

    added endpoint /jobs/search to search for jobs based on title, location, type, level

--- server/src/routes/jobRoutes.js
+++ server/src/routes/jobRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { getJobs } = require('../controllers/jobController');
+const { getJobs, searchJobsController } = require('../controllers/jobController');
 
 const jobRoutes = express.Router();
 
 // Route to get jobs
 jobRoutes.get('/home', getJobs);
+jobRoutes.get('/search', searchJobsController);
 
 module.exports = jobRoutes;
