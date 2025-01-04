9657808310b59cac9401219070bb0e36cf6e14c0
Oxshady <138263957+Oxshady@users.noreply.github.com>
2025-01-04 17:05:40 +0200

    using the absolute path not relativepath

--- server/src/utils/downloadPdf.js
+++ server/src/utils/downloadPdf.js
@@ -1,26 +1,32 @@
-const axios = require('axios')
-const fs = require('fs')
-const User = require('../models/User')
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+const User = require('../models/User');
+
 const downloadPdf = async(id) => {
     try {
-        const user = await User.findById(id)
-        if (!user.cvFile) {
-            return null
+        const user = await User.findById(id);
+        if (!user || !user.cvFile) {
+            return null;
         }
-        const fileUrl = user.cvFile;
-        const filePath = `../PDF/${id}.pdf`
-        const response = await axios.get(fileUrl, { responseType: 'stream' });
 
+        const fileUrl = user.cvFile;
+        const filePath = path.resolve(__dirname, '../PDF', `${id}.pdf`);
+
+        const response = await axios.get(fileUrl, { responseType: 'stream' });
         const writer = fs.createWriteStream(filePath);
+
         response.data.pipe(writer);
 
         await new Promise((resolve, reject) => {
             writer.on('finish', resolve);
             writer.on('error', reject);
-        })
-        return filePath
+        });
+
+        console.log(`File downloaded successfully to: ${filePath}`);
+        return filePath;
     } catch (err) {
-        return err
+        return undefined
     }
 };
 
