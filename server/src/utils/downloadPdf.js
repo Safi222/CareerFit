a06beaf6fc2a0683a24078c71f047294a8f2f64d
Oxshady <138263957+Oxshady@users.noreply.github.com>
2025-01-04 07:55:39 +0200

    update: make download path return the file path

--- server/src/utils/downloadPdf.js
+++ server/src/utils/downloadPdf.js
@@ -4,19 +4,20 @@ const cloudinary = require('../config/cloudinary');
 
 const downloadPdf = async(publicId) => {
     try {
+        const filePath = `../PDF/${publicId}`
         const fileUrl = cloudinary.url(publicId, { resource_type: 'raw' });
         const response = await axios.get(fileUrl, { responseType: 'stream' });
 
-        const writer = fs.createWriteStream(`../PDF/${publicId}`);
+        const writer = fs.createWriteStream(filePath);
         response.data.pipe(writer);
 
         await new Promise((resolve, rejects) => {
             writer.on('finish', resolve);
             writer.on('error', rejects);
         })
-        return true
+        return filePath
     } catch (err) {
-        return false
+        return null
     }
 };
 
