5d17118e890beb33fb5e3494c2972f6752ae3156
Oxshady <138263957+Oxshady@users.noreply.github.com>
2025-01-05 13:33:40 +0200

    refactor: making code follow one standard(Jsend)

--- server/src/controllers/jobController.js
+++ server/src/controllers/jobController.js
@@ -5,30 +5,27 @@ const { fetchJobs, searchJobs } = require('../services/jobService');
  * @param {object} req - Express request object.
  * @param {object} res - Express response object.
  */
-const getJobs = async (req, res) => {
-  try {
-    const query = req.query.query || 'tech jobs in Egypt'; // Default query
-	const page = req.query.page;
-	const num_pages = req.query.num_pages
-    const jobs = await fetchJobs(query, page, num_pages);
-    return res.status(200).json(
-      {
-        status: "success",
-        data: {
-          jobs
-        }
-      });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json(
-			{
-				status: "fail",
-				data: {
-					msg: 'Failed to fetch jobs'
-				}
-			}
-		)
-  }
+const getJobs = async(req, res) => {
+    try {
+        const query = req.query.query || 'tech jobs in Egypt'; // Default query
+        const page = req.query.page;
+        const num_pages = req.query.num_pages
+        const jobs = await fetchJobs(query, page, num_pages);
+        return res.status(200).json({
+            status: "success",
+            data: {
+                jobs
+            }
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            status: "fail",
+            data: {
+                title: 'Failed to fetch jobs'
+            }
+        })
+    }
 };
 
 /**
@@ -36,31 +33,28 @@ const getJobs = async (req, res) => {
  * @param {object} req - Express request object.
  * @param {object} res - Express response object.
  */
-const searchJobsController = async (req, res) => {
+const searchJobsController = async(req, res) => {
     try {
         const { title, location, type, level, page, num_pages } = req.query;
 
         // Construct filters object
-		const query = `${title?title:''} ${location?location:''} ${type?type:''} ${level?level:''}`
+        const query = `${title?title:''} ${location?location:''} ${type?type:''} ${level?level:''}`
 
         const jobs = await searchJobs(query, page, num_pages);
-        return res.status(200).json(
-          {
+        return res.status(200).json({
             status: "success",
             data: {
-              jobs
+                jobs
             }
-          });
+        });
     } catch (error) {
         console.error(error);
-        return res.status(500).json(
-          {
+        return res.status(500).json({
             status: "fail",
             data: {
-              msg: 'Failed to fetch jobs'
+                title: 'Failed to fetch jobs'
             }
-          }
-        )
+        })
     }
 };
 
