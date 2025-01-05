b9d4b043436f83e0e870b2565511a6b7a6215849
Oxshady <138263957+Oxshady@users.noreply.github.com>
2025-01-05 17:19:40 +0200

    integrate profile controller with user route

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -3,10 +3,11 @@ const { uploadPic, uploadcv } = require('../middlewares/fileUpload');
 const { uploadProfilePicture, uploadCV } = require('../controllers/userController');
 const { authorizationVerfication } = require('../middlewares/authMiddleware');
 const detectMiddleware = require('../middlewares/detectMalware')
+const profileController = require('../controllers/profileController')
 const userRoutes = express.Router();
 
 // Protect routes with authorizationVerfication
 userRoutes.post('/profile-picture', authorizationVerfication, uploadPic.single('profilePic'), detectMiddleware, uploadProfilePicture);
 userRoutes.post('/cv', authorizationVerfication, uploadcv.single('cvFile'), detectMiddleware, uploadCV);
-
+userRoutes.get('/profile', authorizationVerfication, profileController)
 module.exports = userRoutes;
